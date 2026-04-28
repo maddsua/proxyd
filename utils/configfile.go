3d9c120e5fd7548960f8de95238d3d5de03d750d
maddsua <maddsuadev@pm.me>
2026-04-28 15:11:15 +0200

    update config file extension checks

--- utils/configfile.go
+++ utils/configfile.go
@@ -36,12 +36,20 @@ func LoadConfigLocation[T any](location string) (*T, error) {
 
 	var cfg T
 
-	if ext := path.Ext(location); strings.EqualFold(ext, ".json") {
+	switch strings.ToLower(path.Ext(location)) {
+
+	case ".json":
 		if err := json.NewDecoder(file).Decode(&cfg); err != nil {
 			return nil, fmt.Errorf("decode json config: %v", err)
 		}
-	} else if err := yaml.NewDecoder(file).Decode(&cfg); err != nil {
-		return nil, fmt.Errorf("decode yaml config: %v", err)
+
+	case ".yml", ".yaml":
+		if err := yaml.NewDecoder(file).Decode(&cfg); err != nil {
+			return nil, fmt.Errorf("decode yaml config: %v", err)
+		}
+
+	default:
+		return nil, errors.New("unsupported config file type")
 	}
 
 	return &cfg, nil
